4686918224feef0b2cfc990c821db763cc72e69d
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-02 23:10:05 +0200

    added tests for .yaml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,28 +9,49 @@ const __dirname = dirname(__filename);
 let correctDifBetFile1File2;
 let correctDifBetFile1File1;
 let correctDifBetFile3File1;
-let pathToFile1;
-let pathToFile2;
-let pathToFile3;
+let pathToF1j;
+let pathToF1y;
+let pathToF2j;
+let pathToF2y;
+let pathToF3j;
+let pathToF3y;
 
 beforeEach(() => {
   correctDifBetFile1File2 = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
   correctDifBetFile1File1 = '{\n    follow: false\n    host: hexlet.io\n    proxy: 123.234.53.22\n    timeout: 50\n}';
   correctDifBetFile3File1 = '{\n  + follow: false\n  + host: hexlet.io\n  + proxy: 123.234.53.22\n  + timeout: 50\n}';
-  pathToFile1 = path.resolve(__dirname, '..', '__fixtures__', 'file1.json');
-  pathToFile2 = path.resolve(__dirname, '..', '__fixtures__', 'file2.json');
-  pathToFile3 = path.resolve(__dirname, '..', '__fixtures__', 'file3.json');
+ 
+  pathToF1j = path.resolve(__dirname, '..', '__fixtures__', 'file1.json');
+  pathToF2j = path.resolve(__dirname, '..', '__fixtures__', 'file2.json');
+  pathToF3j = path.resolve(__dirname, '..', '__fixtures__', 'file3.json');
+
+  pathToF1y = path.resolve(__dirname, '..', '__fixtures__', 'file1.yaml');
+  pathToF2y = path.resolve(__dirname, '..', '__fixtures__', 'file2.yml');
+  pathToF3y = path.resolve(__dirname, '..', '__fixtures__', 'file3.yaml');
 });
 
-describe('usual Work', () => {
+describe('usual Work with .json', () => {
   test('with relative path', () => {
     expect(gendiff('file1.json', 'file2.json')).toEqual(correctDifBetFile1File2);
     expect(gendiff('file1.json', 'file1.json')).toEqual(correctDifBetFile1File1);
     expect(gendiff('file3.json', 'file1.json')).toEqual(correctDifBetFile3File1);
   });
   test('with absolute path', () => {
-    expect(gendiff(pathToFile1, pathToFile2)).toEqual(correctDifBetFile1File2);
-    expect(gendiff(pathToFile1, pathToFile1)).toEqual(correctDifBetFile1File1);
-    expect(gendiff(pathToFile3, pathToFile1)).toEqual(correctDifBetFile3File1);
+    expect(gendiff(pathToF1j, pathToF2j)).toEqual(correctDifBetFile1File2);
+    expect(gendiff(pathToF1j, pathToF1j)).toEqual(correctDifBetFile1File1);
+    expect(gendiff(pathToF3j, pathToF1j)).toEqual(correctDifBetFile3File1);
+  });
+});
+
+describe('usual Work with .yaml || .yml', () => {
+  test('with relative path', () => {
+    expect(gendiff('file1.yaml', 'file2.yml')).toEqual(correctDifBetFile1File2);
+    expect(gendiff('file1.yaml', 'file1.yaml')).toEqual(correctDifBetFile1File1);
+    expect(gendiff('file3.yaml', 'file1.yaml')).toEqual(correctDifBetFile3File1);
+  });
+  test('with absolute path', () => {
+    expect(gendiff(pathToF1y, pathToF2y)).toEqual(correctDifBetFile1File2);
+    expect(gendiff(pathToF1y, pathToF1y)).toEqual(correctDifBetFile1File1);
+    expect(gendiff(pathToF3y, pathToF1y)).toEqual(correctDifBetFile3File1);
   });
 });
