9e86c4dc60465790a14519949bf04fe16bceb75b
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-04 21:15:58 +0200

    edit bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import gendiff from '../src/gendiff2.js';
-import getValue from '../src/parser.js';
-import stringify from '../src/stylish.js';
+import parser from '../src/parser.js';
+import stylish from '../src/stylish.js';
 
 const program = new Command();
 program
   .version('1.0.0', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', stylish, stylish)
   .helpOption('-h, --help', 'output usage information')
-  .arguments('<file1> <file2>')
-  .action((file1, file2) => {
-    const valueFile1 = getValue(file1);
-    const valueFile2 = getValue(file2);
-    console.log(stringify(gendiff(valueFile1, valueFile1)));
-    return stringify(gendiff(valueFile1, valueFile2));
+  .argument('<value1>', 'FirstFile', parser)
+  .argument('<value2>', 'SecondFile', parser)
+  .action((value1, value2, options) => {
+    const treeDiff = gendiff(value1, value2);
+    console.log(options.format(treeDiff));
+    return options.format(treeDiff);
   });
 program.parse();
