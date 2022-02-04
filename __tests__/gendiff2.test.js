da44c9ff583d0e2d48085a6f0ec7c31aaf05750f
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-04 21:09:41 +0200

    edit test/gendiff2.test.js

--- __tests__/gendiff2.test.js
+++ __tests__/gendiff2.test.js
@@ -1,122 +1,106 @@
-import * as path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import gendiff from '../src/gendiff2.js';
-import stringify from '../src/stylish.js';
-import getValue from '../src/parser.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import stylish from '../src/stylish.js';
+import parser from '../src/parser.js';
 
 let correctDifBetFile1File2;
 let correctDifBetFile1File1;
-let pathToF1j;
-let pathToF1y;
-let pathToF2j;
-let pathToF2y;
-
+let value1j;
+let value1y;
+let value2j;
+let value2y;
 beforeEach(() => {
   correctDifBetFile1File2 = '{\n'
-                          + '    common:\n'
+                          + '    common: {\n'
                           + '      + follow: false\n'
                           + '        setting1: Value 1\n'
                           + '      - setting2: 200\n'
-                          + '      - setting3: null\n'
-                          + '        setiing4: blah blah\n'
+                          + '      - setting3: true\n'
+                          + '      + setting3: null\n'
+                          + '      + setting4: blah blah\n'
                           + '      + setting5: {\n'
                           + '            key5: value5\n'
                           + '        }\n'
                           + '        setting6: {\n'
-                          + '            dog6: {\n'
-                          + '              - wow:\n'
+                          + '            doge: {\n'
+                          + '              - wow: \n'
                           + '              + wow: so much\n'
                           + '            }\n'
                           + '            key: value\n'
                           + '          + ops: vops\n'
                           + '        }\n'
-                          + '     }\n'
-                          + '     group1: {\n'
-                          + '       - baz: bas\n'
-                          + '       + baz: bars\n'
-                          + '         foo: bar\n'
-                          + '       - nest: {\n'
-                          + '             key: value'
-                          + '         }\n'
-                          + '       + nest: str\n'
-                          + '     }\n'
-                          + '   - group2: {\n'
-                          + '         abc: 12345\n'
-                          + '         deep: {\n'
-                          + '             id: 45\n'
-                          + '         }\n'
-                          + '     }\n'
-                          + '   + group3: {\n'
-                          + '         deep: {\n'
-                          + '             id: {\n'
-                          + '                 number; 45\n'
-                          + '             }\n'
-                          + '         }\n'
-                          + '         fee: 100500\n'
-                          + '     }\n'
+                          + '    }\n'
+                          + '    group1: {\n'
+                          + '      - baz: bas\n'
+                          + '      + baz: bars\n'
+                          + '        foo: bar\n'
+                          + '      - nest: {\n'
+                          + '            key: value\n'
+                          + '        }\n'
+                          + '      + nest: str\n'
+                          + '    }\n'
+                          + '  - group2: {\n'
+                          + '        abc: 12345\n'
+                          + '        deep: {\n'
+                          + '            id: 45\n'
+                          + '        }\n'
+                          + '    }\n'
+                          + '  + group3: {\n'
+                          + '        deep: {\n'
+                          + '            id: {\n'
+                          + '                number: 45\n'
+                          + '            }\n'
+                          + '        }\n'
+                          + '        fee: 100500\n'
+                          + '    }\n'
                           + '}';
 
   correctDifBetFile1File1 = '{\n'
-                          + '    common:\n'
-                          + '        follow: false\n'
+                          + '    common: {\n'
                           + '        setting1: Value 1\n'
                           + '        setting2: 200\n'
-                          + '        setting3: null\n'
+                          + '        setting3: true\n'
                           + '        setting6: {\n'
-                          + '            dog6: {\n'
-                          + '                wow:\n'
+                          + '            doge: {\n'
+                          + '                wow: \n'
                           + '            }\n'
                           + '            key: value\n'
                           + '        }\n'
-                          + '     }\n'
-                          + '     group1: {\n'
-                          + '         baz: bas\n'
-                          + '         foo: bar\n'
-                          + '         nest: {\n'
-                          + '             key: value'
-                          + '         }\n'
-                          + '     }\n'
-                          + '     group2: {\n'
-                          + '         abc: 12345\n'
-                          + '         deep: {\n'
-                          + '             id: 45\n'
-                          + '         }\n'
-                          + '     }\n'
+                          + '    }\n'
+                          + '    group1: {\n'
+                          + '        baz: bas\n'
+                          + '        foo: bar\n'
+                          + '        nest: {\n'
+                          + '            key: value\n'
+                          + '        }\n'
+                          + '    }\n'
+                          + '    group2: {\n'
+                          + '        abc: 12345\n'
+                          + '        deep: {\n'
+                          + '            id: 45\n'
+                          + '        }\n'
+                          + '    }\n'
                           + '}';
 
-  pathToF1j = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file1.json');
-  pathToF2j = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file2.json');
-
-  pathToF1y = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file1.yaml');
-  pathToF2y = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file2.yaml');
+  value1j = parser('file1.json');
+  value2j = parser('file2.json');
+  value1y = parser('file1.yaml');
+  value2y = parser('file2.yaml');
 });
 
-describe('usual Work with .json', () => {
-  test('with relative path', () => {
-    expect(stringify(gendiff(getValue('file1.json'), getValue('file2.json')))).toEqual(correctDifBetFile1File2);
-    expect(stringify(gendiff('file1.json', 'file1.json'))).toEqual(correctDifBetFile1File1);
-    // expect(gendiff('file3.json', 'file1.json')).toEqual(correctDifBetFile3File1);
+describe('usual Work', () => {
+  test('compaire json files', () => {
+    expect(stylish(gendiff(value1j, value2j))).toEqual(correctDifBetFile1File2);
+    expect(stylish(gendiff(value1j, value1j))).toEqual(correctDifBetFile1File1);
   });
-  test('with absolute path', () => {
-    expect(stringify(gendiff(pathToF1j, pathToF2j))).toEqual(correctDifBetFile1File2);
-    expect(stringify(gendiff(pathToF1j, pathToF1j))).toEqual(correctDifBetFile1File1);
-    // expect(gendiff(pathToF3j, pathToF1j)).toEqual(correctDifBetFile3File1);
+  test('compaire yaml files', () => {
+    expect(stylish(gendiff(value1y, value2y))).toEqual(correctDifBetFile1File2);
+    expect(stylish(gendiff(value1y, value1y))).toEqual(correctDifBetFile1File1);
   });
 });
 
-describe('usual Work with .yaml || .yml', () => {
-  test('with relative path', () => {
-    expect(stringify(gendiff(getValue('file1.yaml'), getValue('file2.yaml')))).toEqual(correctDifBetFile1File2);
-    expect(stringify(gendiff(getValue('file1.yaml'), getValue('file1.yaml')))).toEqual(correctDifBetFile1File1);
-    // expect(gendiff('file3.yaml', 'file1.yaml')).toEqual(correctDifBetFile3File1);
-  });
-  test('with absolute path', () => {
-    expect(stringify(gendiff(pathToF1y, pathToF2y))).toEqual(correctDifBetFile1File2);
-    expect(stringify(gendiff(pathToF1y, pathToF1y))).toEqual(correctDifBetFile1File1);
-    // expect(gendiff(pathToF3y, pathToF1y)).toEqual(correctDifBetFile3File1);
+describe('Unusual Work', () => {
+  test('compaire json with yaml', () => {
+    expect(stylish(gendiff(value1j, value2y))).toEqual(correctDifBetFile1File2);
+    expect(stylish(gendiff(value1j, value1y))).toEqual(correctDifBetFile1File1);
   });
 });
