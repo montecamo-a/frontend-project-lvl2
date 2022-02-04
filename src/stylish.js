94bd670107ff3b34a52472740a71b28a19b9cdea
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-04 21:10:32 +0200

    edit stylish.js

--- src/stylish.js
+++ src/stylish.js
@@ -1,12 +1,16 @@
 import _ from 'lodash';
 
-const stringify = (value, replacer = ' ', countReplace = 4) => {
+const stylish = (value, replacer = ' ', countReplace = 4) => {
   const goToDepth = (value1, depth = 1) => {
     if (!_.isObject(value1)) return `${value1}`;
 
     const currentReplacerLength = countReplace * depth;
-    const getStrings = Object.entries(value1).map(([key, value2]) => `\n${replacer.repeat(currentReplacerLength - 2)}${key}: ${goToDepth(value2, depth + 1)}`);
-
+    const getStrings = Object.entries(value1).map(([key, value2]) => {
+      let trimLengthReplacer = 2;
+      if (!key.startsWith('+') && !key.startsWith('-')) trimLengthReplacer = 0;
+      const result = `\n${replacer.repeat(currentReplacerLength - trimLengthReplacer)}${key}: ${goToDepth(value2, depth + 1)}`;
+      return result;
+    });
     const getresult = getStrings.reduce((acc, str, index, array) => {
       if (index === (array.length - 1)) return `${acc}${str}\n${replacer.repeat(currentReplacerLength - countReplace)}}`;
       return acc + str;
@@ -16,4 +20,4 @@ const stringify = (value, replacer = ' ', countReplace = 4) => {
   };
   return goToDepth(value);
 };
-export default stringify;
+export default stylish;
