52a4ab549230a4857b63baea77a2905db3bf9f05
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-05 16:09:49 +0200

    have added formatName into module gendiff

--- src/gendiff2.js
+++ src/gendiff2.js
@@ -1,32 +1,69 @@
 import _ from 'lodash';
+import stylish from './stylish.js';
+import parser from './parser.js';
 
-const gendiff = (node1, node2) => {
-  if (!_.isObject(node1) || !_.isObject(node2)) {
-    if (node1 === node2) return false;
-    return true;
-  }
+const gendiff = (filepath1, filepath2, formatName = stylish) => {
+  const parsedValueOfTheFirstFile = parser(filepath1);
+  const parsedValueOfTheSecondFile = parser(filepath2);
 
-  const [keysOfObj1, keysOfObj2] = [Object.keys(node1), Object.keys(node2)];
-  const allKeys = _.uniq([...keysOfObj1, ...keysOfObj2]).sort();
-
-  const resultTree = allKeys.reduce((acc, key) => {
-    if (_.has(node1, key) && _.has(node2, key)) {
-      if (_.isObjectLike(node1[key]) && _.isObjectLike(node2[key])) {
-        acc[`${key}`] = gendiff(node1[key], node2[key]);
-        return acc;
-      }
-      const areChanged = gendiff(node1[key], node2[key]);
-      if (!areChanged) {
-        acc[`${key}`] = node1[key];
-        return acc;
-      }
+  const diff = (nodeOfFirstFile, nodeOfSecondFile) => {
+    if (!_.isObject(nodeOfFirstFile) || !_.isObject(nodeOfSecondFile)) {
+      if (nodeOfFirstFile === nodeOfSecondFile) return true;
+      return false;
     }
-    if (_.has(node1, key)) acc[`- ${key}`] = node1[key];
-    if (_.has(node2, key)) acc[`+ ${key}`] = node2[key];
+
+    const keysOfNodeOfFirstFile = Object.keys(nodeOfFirstFile);
+    const keysOfNodeOfSecondFile = Object.keys(nodeOfSecondFile);
+    const allKeys = _.uniq([...keysOfNodeOfFirstFile, ...keysOfNodeOfSecondFile]).sort();
+
+    const treeOfDiff = allKeys.reduce((acc, key) => {
+      if (_.has(nodeOfFirstFile, key) && _.has(nodeOfSecondFile, key)) {
+        if (_.isObjectLike(nodeOfFirstFile[key]) && _.isObjectLike(nodeOfSecondFile[key])) {
+          acc[`${key}`] = diff(nodeOfFirstFile[key], nodeOfSecondFile[key]);
+          return acc;
+        }
+
+        const areUnchanged = diff(nodeOfFirstFile[key], nodeOfSecondFile[key]);
+        if (areUnchanged) {
+          acc[`${key}`] = nodeOfFirstFile[key];
+          return acc;
+        }
+      }
+
+      if (_.has(nodeOfFirstFile, key)) acc[`- ${key}`] = nodeOfFirstFile[key];
+      if (_.has(nodeOfSecondFile, key)) acc[`+ ${key}`] = nodeOfSecondFile[key];
+      return acc;
+    }, {});
+
+    return treeOfDiff;
+  };
+
+  return formatName(diff(parsedValueOfTheFirstFile, parsedValueOfTheSecondFile));
+};
+
+const getStatus = (key) => {
+  if (key.startsWith('+')) return 'added';
+  if (key.startsWith('-')) return 'deleted';
+  return 'unchanged';
+};
+
+const getAsArrayOfKeys = (tree) => Object.keys(tree);
+
+const getAsArrayKeysAndValues = (tree) => Object.entries(tree);
+
+const getValue = (value) => {
+  if (!_.isObject(value)) return value;
+
+  const obj = getAsArrayKeysAndValues(value).reduce((acc, [key, value2]) => {
+    acc[key] = getValue(value2);
     return acc;
   }, {});
 
-  return resultTree;
+  return obj;
 };
 
-export default gendiff;
+export {
+  gendiff, getStatus,
+  getAsArrayKeysAndValues,
+  getValue, getAsArrayOfKeys,
+};
