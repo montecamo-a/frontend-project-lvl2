910584f5071f427315ecb2791a03bcba71c6d5ee
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-06 05:29:05 +0200

    added formatter json into index.js

--- formatters/index.js
+++ formatters/index.js
@@ -1,10 +1,12 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const getFormatter = (nameOfFormatter) => {
   let formatter;
   if (nameOfFormatter === 'plain') formatter = plain;
   if (nameOfFormatter === 'stylish') formatter = stylish;
+  if (nameOfFormatter === 'json') formatter = json;
   return formatter;
 };
 export default getFormatter;
