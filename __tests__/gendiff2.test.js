d4114b778252be3140763e4f7b6f90fefa5ecd9d
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-06 04:33:35 +0200

    added tests for seventh step

--- __tests__/gendiff2.test.js
+++ __tests__/gendiff2.test.js
@@ -5,122 +5,170 @@ import gendiff from '../src/gendiff2.js';
 
 let __filename;
 let __dirname;
-let correctDifBetweenFile1File2;
-let correctDifBetweenFile1File1;
-let pathToFile1json;
-let pathToFile2json;
-let pathToFile1yaml;
-let pathToFile2yaml;
+let firstCorrectDifWhenUseStylish;
+let firstCorrectDifWhenUsePlain;
+let secondCorrectDifWhenUseStylish;
+let secondCorrectDifWhenUsePlain;
+let pathToFile1j;
+let pathToFile2j;
+let pathToFile1y;
+let pathToFile2y;
 
-beforeEach(() => {
+beforeAll(() => {
   __filename = fileURLToPath(import.meta.url);
   __dirname = dirname(__filename);
-
-  correctDifBetweenFile1File2 = '{\n'
-                          + '    common: {\n'
-                          + '      + follow: false\n'
-                          + '        setting1: Value 1\n'
-                          + '      - setting2: 200\n'
-                          + '      - setting3: true\n'
-                          + '      + setting3: null\n'
-                          + '      + setting4: blah blah\n'
-                          + '      + setting5: {\n'
-                          + '            key5: value5\n'
-                          + '        }\n'
-                          + '        setting6: {\n'
-                          + '            doge: {\n'
-                          + '              - wow: \n'
-                          + '              + wow: so much\n'
-                          + '            }\n'
-                          + '            key: value\n'
-                          + '          + ops: vops\n'
-                          + '        }\n'
-                          + '    }\n'
-                          + '    group1: {\n'
-                          + '      - baz: bas\n'
-                          + '      + baz: bars\n'
-                          + '        foo: bar\n'
-                          + '      - nest: {\n'
-                          + '            key: value\n'
-                          + '        }\n'
-                          + '      + nest: str\n'
-                          + '    }\n'
-                          + '  - group2: {\n'
-                          + '        abc: 12345\n'
-                          + '        deep: {\n'
-                          + '            id: 45\n'
-                          + '        }\n'
-                          + '    }\n'
-                          + '  + group3: {\n'
-                          + '        deep: {\n'
-                          + '            id: {\n'
-                          + '                number: 45\n'
-                          + '            }\n'
-                          + '        }\n'
-                          + '        fee: 100500\n'
-                          + '    }\n'
-                          + '}';
-
-  correctDifBetweenFile1File1 = '{\n'
-                          + '    common: {\n'
-                          + '        setting1: Value 1\n'
-                          + '        setting2: 200\n'
-                          + '        setting3: true\n'
-                          + '        setting6: {\n'
-                          + '            doge: {\n'
-                          + '                wow: \n'
-                          + '            }\n'
-                          + '            key: value\n'
-                          + '        }\n'
-                          + '    }\n'
-                          + '    group1: {\n'
-                          + '        baz: bas\n'
-                          + '        foo: bar\n'
-                          + '        nest: {\n'
-                          + '            key: value\n'
-                          + '        }\n'
-                          + '    }\n'
-                          + '    group2: {\n'
-                          + '        abc: 12345\n'
-                          + '        deep: {\n'
-                          + '            id: 45\n'
-                          + '        }\n'
-                          + '    }\n'
-                          + '}';
-
-  pathToFile1json = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file1.json');
-  pathToFile2json = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file2.json');
-  pathToFile1yaml = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file1.yaml');
-  pathToFile2yaml = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file2.yaml');
+  pathToFile1j = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file1.json');
+  pathToFile2j = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file2.json');
+  pathToFile1y = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file1.yaml');
+  pathToFile2y = path.resolve(__dirname, '..', '__fixtures__/recursionVolumes', 'file2.yaml');
 });
 
-describe('Usual work with the relative paths', () => {
-  test('compaire json files', () => {
-    expect(gendiff('file1.json', 'file2.json')).toEqual(correctDifBetweenFile1File2);
-    expect(gendiff('file1.json', 'file1.json')).toEqual(correctDifBetweenFile1File1);
+describe('When formateName = stylish', () => {
+  beforeEach(() => {
+    firstCorrectDifWhenUseStylish = '{\n'
+                            + '    common: {\n'
+                            + '      + follow: false\n'
+                            + '        setting1: Value 1\n'
+                            + '      - setting2: 200\n'
+                            + '      - setting3: true\n'
+                            + '      + setting3: null\n'
+                            + '      + setting4: blah blah\n'
+                            + '      + setting5: {\n'
+                            + '            key5: value5\n'
+                            + '        }\n'
+                            + '        setting6: {\n'
+                            + '            doge: {\n'
+                            + '              - wow: \n'
+                            + '              + wow: so much\n'
+                            + '            }\n'
+                            + '            key: value\n'
+                            + '          + ops: vops\n'
+                            + '        }\n'
+                            + '    }\n'
+                            + '    group1: {\n'
+                            + '      - baz: bas\n'
+                            + '      + baz: bars\n'
+                            + '        foo: bar\n'
+                            + '      - nest: {\n'
+                            + '            key: value\n'
+                            + '        }\n'
+                            + '      + nest: str\n'
+                            + '    }\n'
+                            + '  - group2: {\n'
+                            + '        abc: 12345\n'
+                            + '        deep: {\n'
+                            + '            id: 45\n'
+                            + '        }\n'
+                            + '    }\n'
+                            + '  + group3: {\n'
+                            + '        deep: {\n'
+                            + '            id: {\n'
+                            + '                number: 45\n'
+                            + '            }\n'
+                            + '        }\n'
+                            + '        fee: 100500\n'
+                            + '    }\n'
+                            + '}';
+
+    secondCorrectDifWhenUseStylish = '{\n'
+                            + '    common: {\n'
+                            + '        setting1: Value 1\n'
+                            + '        setting2: 200\n'
+                            + '        setting3: true\n'
+                            + '        setting6: {\n'
+                            + '            doge: {\n'
+                            + '                wow: \n'
+                            + '            }\n'
+                            + '            key: value\n'
+                            + '        }\n'
+                            + '    }\n'
+                            + '    group1: {\n'
+                            + '        baz: bas\n'
+                            + '        foo: bar\n'
+                            + '        nest: {\n'
+                            + '            key: value\n'
+                            + '        }\n'
+                            + '    }\n'
+                            + '    group2: {\n'
+                            + '        abc: 12345\n'
+                            + '        deep: {\n'
+                            + '            id: 45\n'
+                            + '        }\n'
+                            + '    }\n'
+                            + '}';
   });
-  test('compaire yaml files', () => {
-    expect(gendiff('file1.yaml', 'file2.yaml')).toEqual(correctDifBetweenFile1File2);
-    expect(gendiff('file1.yaml', 'file1.yaml')).toEqual(correctDifBetweenFile1File1);
+
+  describe('Main work', () => {
+    test('Compaire json files when have relative paths', () => {
+      expect(gendiff('file1.json', 'file2.json')).toEqual(firstCorrectDifWhenUseStylish);
+      expect(gendiff('file1.json', 'file1.json')).toEqual(secondCorrectDifWhenUseStylish);
+    });
+    test('Compaire yaml files when have relative paths', () => {
+      expect(gendiff('file1.yaml', 'file2.yaml')).toEqual(firstCorrectDifWhenUseStylish);
+      expect(gendiff('file1.yaml', 'file1.yaml')).toEqual(secondCorrectDifWhenUseStylish);
+    });
+    test('Compaire json files when have absolute paths', () => {
+      expect(gendiff(pathToFile1j, pathToFile2j)).toEqual(firstCorrectDifWhenUseStylish);
+      expect(gendiff(pathToFile1j, pathToFile1j)).toEqual(secondCorrectDifWhenUseStylish);
+    });
+    test('Compaire yaml files when have absolute paths', () => {
+      expect(gendiff(pathToFile1y, pathToFile2y)).toEqual(firstCorrectDifWhenUseStylish);
+      expect(gendiff(pathToFile1y, pathToFile1y)).toEqual(secondCorrectDifWhenUseStylish);
+    });
+  });
+
+  describe('Additional work', () => {
+    test('Compaire json with yaml files when have realative paths', () => {
+      expect(gendiff('file1.json', 'file2.yaml')).toEqual(firstCorrectDifWhenUseStylish);
+    });
+    test('Compaire json with yaml files when have absolute paths', () => {
+      expect(gendiff(pathToFile1y, pathToFile2y)).toEqual(firstCorrectDifWhenUseStylish);
+    });
   });
 });
 
-describe('Usual work with the absolute paths', () => {
-  test('compaire json files', () => {
-    expect(gendiff(pathToFile1json, pathToFile2json)).toEqual(correctDifBetweenFile1File2);
-    expect(gendiff(pathToFile1json, pathToFile1json)).toEqual(correctDifBetweenFile1File1);
-  });
-  test('compaire yaml files', () => {
-    expect(gendiff(pathToFile1yaml, pathToFile2yaml)).toEqual(correctDifBetweenFile1File2);
-    expect(gendiff(pathToFile1yaml, pathToFile1yaml)).toEqual(correctDifBetweenFile1File1);
-  });
-});
+describe('When formateName = plain', () => {
+  beforeEach(() => {
+    firstCorrectDifWhenUsePlain = "Property 'common.follow' was added with value: false\n"
+                                  + "Property 'common.setting2' was removed\n"
+                                  + "Property 'common.setting3' was updated. From true to null\n"
+                                  + "Property 'common.setting4' was added with value: 'blah blah'\n"
+                                  + "Property 'common.setting5' was added with value: [complex value]\n"
+                                  + "Property 'common.setting6.doge.wow' was updated. From '' to 'so much'\n"
+                                  + "Property 'common.setting6.ops' was added with value: 'vops'\n"
+                                  + "Property 'group1.baz' was updated. From 'bas' to 'bars'\n"
+                                  + "Property 'group1.nest' was updated. From [complex value] to 'str'\n"
+                                  + "Property 'group2' was removed\n"
+                                  + "Property 'group3' was added with value: [complex value]\n";
 
-describe('Unusual Work - compaire different formats', () => {
-  test('compaire json with yaml files (with realative paths)', () => {
-    expect(gendiff('file1.json', 'file2.yaml')).toEqual(correctDifBetweenFile1File2);
+    secondCorrectDifWhenUsePlain = '';
   });
-  test('compaire json with yaml files (with absolute paths)', () => {
-    expect(gendiff(pathToFile1yaml, pathToFile2yaml)).toEqual(correctDifBetweenFile1File2);
+
+  describe('Main work', () => {
+    test('Compaire json files when have relative paths', () => {
+      expect(gendiff('file1.json', 'file2.json', 'plain')).toEqual(firstCorrectDifWhenUsePlain);
+      expect(gendiff('file1.json', 'file1.json', 'plain')).toEqual(secondCorrectDifWhenUsePlain);
+    });
+    test('Compaire yaml files when have relative paths', () => {
+      expect(gendiff('file1.yaml', 'file2.yaml', 'plain')).toEqual(firstCorrectDifWhenUsePlain);
+      expect(gendiff('file1.yaml', 'file1.yaml', 'plain')).toEqual(secondCorrectDifWhenUsePlain);
+    });
+    test('Compaire json files when have absolute paths', () => {
+      expect(gendiff(pathToFile1j, pathToFile2j, 'plain')).toEqual(firstCorrectDifWhenUsePlain);
+      expect(gendiff(pathToFile1j, pathToFile1j, 'plain')).toEqual(secondCorrectDifWhenUsePlain);
+    });
+    test('Compaire yaml files when have absolute paths', () => {
+      expect(gendiff(pathToFile1y, pathToFile2y, 'plain')).toEqual(firstCorrectDifWhenUsePlain);
+      expect(gendiff(pathToFile1y, pathToFile1y, 'plain')).toEqual(secondCorrectDifWhenUsePlain);
+    });
+  });
+
+  describe('Additional work', () => {
+    test('Compaire json with yaml files when have realative paths', () => {
+      expect(gendiff('file1.json', 'file2.yaml', 'plain')).toEqual(firstCorrectDifWhenUsePlain);
+    });
+    test('Compaire json with yaml files when have absolute paths', () => {
+      expect(gendiff(pathToFile1y, pathToFile2y, 'plain')).toEqual(firstCorrectDifWhenUsePlain);
+    });
   });
 });
