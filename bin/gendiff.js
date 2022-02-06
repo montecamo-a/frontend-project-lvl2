8a48631efd4db2ea59a555056fc26d222cbe6ea3
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-06 04:37:58 +0200

    edit default value of --format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import gendiff from '../src/gendiff2.js';
-import stylish from '../src/stylish.js';
 
 const program = new Command();
 program
   .version('1.0.0', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format', stylish, stylish)
+  .option('-f, --format <type>', 'output format', 'stylish')
   .helpOption('-h, --help', 'output usage information')
   .argument('<filePath1>', 'Path To First File')
   .argument('<filePath2>', 'Path To Second File')
