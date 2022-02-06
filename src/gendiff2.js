c6d65fe853a338cfc8c8686d599a7db1cbeb703e
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-06 04:37:20 +0200

    creat choise formatter

--- src/gendiff2.js
+++ src/gendiff2.js
@@ -1,12 +1,13 @@
-import stylish from './stylish.js';
+import getFormatter from '../formatters/index.js';
 import parser from './parser.js';
 import { diff } from './diff.js';
 
-const gendiff = (filepath1, filepath2, formatName = stylish) => {
+const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
   const parsedValueOfTheFirstFile = parser(filepath1);
   const parsedValueOfTheSecondFile = parser(filepath2);
   const treeOfDiff = diff(parsedValueOfTheFirstFile, parsedValueOfTheSecondFile);
-  return formatName(treeOfDiff);
+  const formatter = getFormatter(formatName);
+  return formatter(treeOfDiff);
 };
 
 export default gendiff;
