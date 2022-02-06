7bcb242c90e65fbd01f4c2196e148e2ad5f3c6c5
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-06 18:14:56 +0200

    half of test for json

--- __tests__/gendiff2.test.js
+++ __tests__/gendiff2.test.js
@@ -7,6 +7,7 @@ let __filename;
 let __dirname;
 let firstCorrectDifWhenUseStylish;
 let firstCorrectDifWhenUsePlain;
+let firstCorrectDifWhenUseJest;
 let secondCorrectDifWhenUseStylish;
 let secondCorrectDifWhenUsePlain;
 let pathToFile1j;
@@ -26,76 +27,76 @@ beforeAll(() => {
 describe('When formateName = stylish', () => {
   beforeEach(() => {
     firstCorrectDifWhenUseStylish = '{\n'
-                            + '    common: {\n'
-                            + '      + follow: false\n'
-                            + '        setting1: Value 1\n'
-                            + '      - setting2: 200\n'
-                            + '      - setting3: true\n'
-                            + '      + setting3: null\n'
-                            + '      + setting4: blah blah\n'
-                            + '      + setting5: {\n'
-                            + '            key5: value5\n'
-                            + '        }\n'
-                            + '        setting6: {\n'
-                            + '            doge: {\n'
-                            + '              - wow: \n'
-                            + '              + wow: so much\n'
-                            + '            }\n'
-                            + '            key: value\n'
-                            + '          + ops: vops\n'
-                            + '        }\n'
-                            + '    }\n'
-                            + '    group1: {\n'
-                            + '      - baz: bas\n'
-                            + '      + baz: bars\n'
-                            + '        foo: bar\n'
-                            + '      - nest: {\n'
-                            + '            key: value\n'
-                            + '        }\n'
-                            + '      + nest: str\n'
-                            + '    }\n'
-                            + '  - group2: {\n'
-                            + '        abc: 12345\n'
-                            + '        deep: {\n'
-                            + '            id: 45\n'
-                            + '        }\n'
-                            + '    }\n'
-                            + '  + group3: {\n'
-                            + '        deep: {\n'
-                            + '            id: {\n'
-                            + '                number: 45\n'
-                            + '            }\n'
-                            + '        }\n'
-                            + '        fee: 100500\n'
-                            + '    }\n'
-                            + '}';
+                                  + '    common: {\n'
+                                  + '      + follow: false\n'
+                                  + '        setting1: Value 1\n'
+                                  + '      - setting2: 200\n'
+                                  + '      - setting3: true\n'
+                                  + '      + setting3: null\n'
+                                  + '      + setting4: blah blah\n'
+                                  + '      + setting5: {\n'
+                                  + '            key5: value5\n'
+                                  + '        }\n'
+                                  + '        setting6: {\n'
+                                  + '            doge: {\n'
+                                  + '              - wow: \n'
+                                  + '              + wow: so much\n'
+                                  + '            }\n'
+                                  + '            key: value\n'
+                                  + '          + ops: vops\n'
+                                  + '        }\n'
+                                  + '    }\n'
+                                  + '    group1: {\n'
+                                  + '      - baz: bas\n'
+                                  + '      + baz: bars\n'
+                                  + '        foo: bar\n'
+                                  + '      - nest: {\n'
+                                  + '            key: value\n'
+                                  + '        }\n'
+                                  + '      + nest: str\n'
+                                  + '    }\n'
+                                  + '  - group2: {\n'
+                                  + '        abc: 12345\n'
+                                  + '        deep: {\n'
+                                  + '            id: 45\n'
+                                  + '        }\n'
+                                  + '    }\n'
+                                  + '  + group3: {\n'
+                                  + '        deep: {\n'
+                                  + '            id: {\n'
+                                  + '                number: 45\n'
+                                  + '            }\n'
+                                  + '        }\n'
+                                  + '        fee: 100500\n'
+                                  + '    }\n'
+                                  + '}';
 
     secondCorrectDifWhenUseStylish = '{\n'
-                            + '    common: {\n'
-                            + '        setting1: Value 1\n'
-                            + '        setting2: 200\n'
-                            + '        setting3: true\n'
-                            + '        setting6: {\n'
-                            + '            doge: {\n'
-                            + '                wow: \n'
-                            + '            }\n'
-                            + '            key: value\n'
-                            + '        }\n'
-                            + '    }\n'
-                            + '    group1: {\n'
-                            + '        baz: bas\n'
-                            + '        foo: bar\n'
-                            + '        nest: {\n'
-                            + '            key: value\n'
-                            + '        }\n'
-                            + '    }\n'
-                            + '    group2: {\n'
-                            + '        abc: 12345\n'
-                            + '        deep: {\n'
-                            + '            id: 45\n'
-                            + '        }\n'
-                            + '    }\n'
-                            + '}';
+                                   + '    common: {\n'
+                                   + '        setting1: Value 1\n'
+                                   + '        setting2: 200\n'
+                                   + '        setting3: true\n'
+                                   + '        setting6: {\n'
+                                   + '            doge: {\n'
+                                   + '                wow: \n'
+                                   + '            }\n'
+                                   + '            key: value\n'
+                                   + '        }\n'
+                                   + '    }\n'
+                                   + '    group1: {\n'
+                                   + '        baz: bas\n'
+                                   + '        foo: bar\n'
+                                   + '        nest: {\n'
+                                   + '            key: value\n'
+                                   + '        }\n'
+                                   + '    }\n'
+                                   + '    group2: {\n'
+                                   + '        abc: 12345\n'
+                                   + '        deep: {\n'
+                                   + '            id: 45\n'
+                                   + '        }\n'
+                                   + '    }\n'
+                                   + '}';
   });
 
   describe('Main work', () => {
@@ -130,16 +131,16 @@ describe('When formateName = stylish', () => {
 describe('When formateName = plain', () => {
   beforeEach(() => {
     firstCorrectDifWhenUsePlain = "Property 'common.follow' was added with value: false\n"
-                                  + "Property 'common.setting2' was removed\n"
-                                  + "Property 'common.setting3' was updated. From true to null\n"
-                                  + "Property 'common.setting4' was added with value: 'blah blah'\n"
-                                  + "Property 'common.setting5' was added with value: [complex value]\n"
-                                  + "Property 'common.setting6.doge.wow' was updated. From '' to 'so much'\n"
-                                  + "Property 'common.setting6.ops' was added with value: 'vops'\n"
-                                  + "Property 'group1.baz' was updated. From 'bas' to 'bars'\n"
-                                  + "Property 'group1.nest' was updated. From [complex value] to 'str'\n"
-                                  + "Property 'group2' was removed\n"
-                                  + "Property 'group3' was added with value: [complex value]\n";
+                                + "Property 'common.setting2' was removed\n"
+                                + "Property 'common.setting3' was updated. From true to null\n"
+                                + "Property 'common.setting4' was added with value: 'blah blah'\n"
+                                + "Property 'common.setting5' was added with value: [complex value]\n"
+                                + "Property 'common.setting6.doge.wow' was updated. From '' to 'so much'\n"
+                                + "Property 'common.setting6.ops' was added with value: 'vops'\n"
+                                + "Property 'group1.baz' was updated. From 'bas' to 'bars'\n"
+                                + "Property 'group1.nest' was updated. From [complex value] to 'str'\n"
+                                + "Property 'group2' was removed\n"
+                                + "Property 'group3' was added with value: [complex value]\n";
 
     secondCorrectDifWhenUsePlain = '';
   });
@@ -160,6 +161,77 @@ describe('When formateName = plain', () => {
     test('Compaire yaml files when have absolute paths', () => {
       expect(gendiff(pathToFile1y, pathToFile2y, 'plain')).toEqual(firstCorrectDifWhenUsePlain);
       expect(gendiff(pathToFile1y, pathToFile1y, 'plain')).toEqual(secondCorrectDifWhenUsePlain);
+    });
+  });
+
+  describe('Additional work', () => {
+    test('Compaire json with yaml files when have realative paths', () => {
+      expect(gendiff('file1.json', 'file2.yaml', 'plain')).toEqual(firstCorrectDifWhenUsePlain);
+    });
+    test('Compaire json with yaml files when have absolute paths', () => {
+      expect(gendiff(pathToFile1y, pathToFile2y, 'plain')).toEqual(firstCorrectDifWhenUsePlain);
+    });
+  });
+});
+
+describe('When formateName = json', () => {
+  beforeEach(() => {
+    firstCorrectDifWhenUseJest = {
+      common: {
+        status: 'unchanged',
+        value: {
+          follow: { status: 'added', value: false },
+          setting1: { status: 'unchanged', value: 'Value' },
+          setting2: { status: 'removed', value: 200 },
+          setting3: { status: 'updated', value: { removed: true, added: null } },
+          setting4: { status: 'added', value: 'blah blah' },
+          setting5: { status: 'added', value: { key5: 'value5' } },
+          setting6: {
+            status: 'unchanged',
+            value: {
+              doge: {
+                status: 'unchanged',
+                value: {
+                  wow: { status: 'updated', value: { removed: '', added: 'so much' } },
+                },
+              },
+              key: { status: 'unchanged', value: 'value' },
+              ops: { status: 'added', value: 'vops' },
+            },
+          },
+        },
+      },
+      group1: {
+        status: 'unchanged',
+        value: {
+          baz: { status: 'updated', value: { removed: 'bas', added: 'bars' } },
+          foo: { status: 'unchanged', value: 'bar' },
+          nest: { status: 'updated', value: { removed: { key: 'value' }, added: 'str' } },
+        },
+      },
+      group2: { status: 'deleted', value: { abc: 12345, deep: { id: 45 } } },
+      group3: { status: 'added', value: { deep: { id: { number: 45 } }, fee: 100500 } },
+    };
+
+    secondCorrectDifWhenUseJest = '';
+  });
+
+  describe('Main work', () => {
+    test('Compaire json files when have relative paths', () => {
+      expect(gendiff('file1.json', 'file2.json', 'jest')).toEqual(firstCorrectDifWhenUseJest);
+      expect(gendiff('file1.json', 'file1.json', 'jest')).toEqual(secondCorrectDifWhenUseJest);
+    });
+    test('Compaire yaml files when have relative paths', () => {
+      expect(gendiff('file1.yaml', 'file2.yaml', 'jest')).toEqual(firstCorrectDifWhenUseJest);
+      expect(gendiff('file1.yaml', 'file1.yaml', 'jest')).toEqual(secondCorrectDifWhenUsePJest);
+    });
+    test('Compaire json files when have absolute paths', () => {
+      expect(gendiff(pathToFile1j, pathToFile2j, 'jest')).toEqual(firstCorrectDifWhenUseJest);
+      expect(gendiff(pathToFile1j, pathToFile1j, 'jest')).toEqual(secondCorrectDifWhenUseJest);
+    });
+    test('Compaire yaml files when have absolute paths', () => {
+      expect(gendiff(pathToFile1y, pathToFile2y, 'jest')).toEqual(firstCorrectDifWhenUseJest);
+      expect(gendiff(pathToFile1y, pathToFile1y, 'jest')).toEqual(secondCorrectDifWhenUseJest);
     });
   });
 
--- formatters/json.js
+++ formatters/json.js
@@ -2,26 +2,28 @@ import _ from 'lodash';
 import {
   getDiffAsObj, getArrayOfKeysStatusesValuesOfFirstLevelNesting,
 } from '../src/diff.js';
-import stylish from './stylish.js';
 
-const json = (diff, format = stylish) => {
+const json = (diff) => {
   const diffAsObj = getDiffAsObj(diff);
 
-  const formatter = (value) => {
-    if (!_.isObject(value)) return `"${value}"`;
+  const formatter = (value, statusOfKey) => {
+    if (!_.isObject(value) || (_.isObject(value) && statusOfKey !== 'unchanged')) {
+      if (typeof (value) === 'string') return `${value}`;
+      return value;
+    }
     const jsonObj = getArrayOfKeysStatusesValuesOfFirstLevelNesting(value)
       .reduce((acc, [key, status, meaning]) => {
         let newKey;
-        if (status === 'updated/added') newKey = `"${_.trimStart(key, '|updated/added|')}"`;
-        if (status === 'updated/deleted') newKey = `"${_.trimStart(key, '|updated/deleted|')}"`;
-        if (status === 'added') newKey = `"${_.trimStart(key, '|added|')}"`;
-        if (status === 'removed') newKey = `"${_.trimStart(key, '|removed|')}"`;
-        if (status === 'unchanged') newKey = `"${key}"`;
-        acc[newKey] = { value: formatter(meaning), status };
+        if (status === 'updated/added') newKey = `${_.trimStart(key, '|updated/added|')}`;
+        if (status === 'updated/removed') newKey = `${_.trimStart(key, '|updated/removed|')}`;
+        if (status === 'added') newKey = `${_.trimStart(key, '|added|')}`;
+        if (status === 'removed') newKey = `${_.trimStart(key, '|removed|')}`;
+        if (status === 'unchanged') newKey = key;
+        acc[newKey] = { value: formatter(meaning, status), status };
         return acc;
       }, {});
     return jsonObj;
   };
-  return format(formatter(diffAsObj));
+  return formatter(diffAsObj);
 };
 export default json;
