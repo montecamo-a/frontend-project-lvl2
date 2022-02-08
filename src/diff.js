e26d56315280c04e9e8f5bcbe10371e7f043e163
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-08 02:08:39 +0200

    edit diff

--- src/diff.js
+++ src/diff.js
@@ -1,57 +1,63 @@
 import _ from 'lodash';
 
-const diff = (nodeOfFirstFile, nodeOfSecondFile) => {
-  if (!_.isObject(nodeOfFirstFile) || !_.isObject(nodeOfSecondFile)) {
-    if (nodeOfFirstFile === nodeOfSecondFile) return true;
-    return false;
-  }
+const diff = (valueOfFirstFile, valueOfSecondFile) => {
+  const getArrayOfDiff = (nodeOfFirstFile, nodeOfSecondFile) => {
+    const checkIsUnchanged = (firstValue, secondValue) => {
+      if (firstValue === secondValue) return false;
+      return true;
+    };
 
-  const keysOfNodeOfFirstFile = Object.keys(nodeOfFirstFile);
-  const keysOfNodeOfSecondFile = Object.keys(nodeOfSecondFile);
-  const allKeys = _.uniq([...keysOfNodeOfFirstFile, ...keysOfNodeOfSecondFile]).sort();
-
-  const treeOfDiff = allKeys.reduce((acc, key) => {
-    if (_.has(nodeOfFirstFile, key) && _.has(nodeOfSecondFile, key)) {
-      if (_.isObjectLike(nodeOfFirstFile[key]) && _.isObjectLike(nodeOfSecondFile[key])) {
-        acc[`${key}`] = diff(nodeOfFirstFile[key], nodeOfSecondFile[key]);
-        return acc;
-      }
-
-      const areUnchanged = diff(nodeOfFirstFile[key], nodeOfSecondFile[key]);
-      if (areUnchanged) {
-        acc[`${key}`] = nodeOfFirstFile[key];
-        return acc;
-      }
-      if (!areUnchanged) {
-        acc[`|updated/removed|${key}`] = nodeOfFirstFile[key];
-        acc[`|updated/added|${key}`] = nodeOfSecondFile[key];
-        return acc;
-      }
+    if (!_.isObject(nodeOfFirstFile) || !_.isObject(nodeOfSecondFile)) {
+      return [nodeOfFirstFile];
     }
 
-    if (_.has(nodeOfFirstFile, key)) acc[`|removed|${key}`] = nodeOfFirstFile[key];
-    if (_.has(nodeOfSecondFile, key)) acc[`|added|${key}`] = nodeOfSecondFile[key];
-    return acc;
-  }, {});
+    const keysOfNodeOfFirstFile = Object.keys(nodeOfFirstFile);
+    const keysOfNodeOfSecondFile = Object.keys(nodeOfSecondFile);
+    const allKeys = _.uniq([...keysOfNodeOfFirstFile, ...keysOfNodeOfSecondFile]).sort();
 
+    const partOfTree = allKeys.reduce((acc, key) => {
+      if (_.has(nodeOfFirstFile, key) && _.has(nodeOfSecondFile, key)) {
+        if (_.isObjectLike(nodeOfFirstFile[key]) && _.isObjectLike(nodeOfSecondFile[key])) {
+          acc.push({ name: `${key}`, status: 'unchanged', childrens: getArrayOfDiff(nodeOfFirstFile[key], nodeOfSecondFile[key]) });
+          return acc;
+        }
+
+        const isUnchanged = checkIsUnchanged(nodeOfFirstFile[key], nodeOfSecondFile[key]);
+        if (!isUnchanged) {
+          acc.push({ name: `${key}`, status: 'unchanged', childrens: getArrayOfDiff(nodeOfFirstFile[key], nodeOfFirstFile[key]) });
+          return acc;
+        }
+        if (isUnchanged) {
+          acc.push({ name: `${key}`, status: 'updated', childrens: [getArrayOfDiff(nodeOfFirstFile[key], nodeOfFirstFile[key]), getArrayOfDiff(nodeOfSecondFile[key], nodeOfSecondFile[key])].flat() });
+          return acc;
+        }
+      }
+
+      if (_.has(nodeOfFirstFile, key)) acc.push({ name: `${key}`, status: 'removed', childrens: getArrayOfDiff(nodeOfFirstFile[key], nodeOfFirstFile[key]) });
+      if (_.has(nodeOfSecondFile, key)) acc.push({ name: `${key}`, status: 'added', childrens: getArrayOfDiff(nodeOfSecondFile[key], nodeOfSecondFile[key]) });
+      return acc;
+    }, []);
+    return partOfTree;
+  };
+  const treeOfDiff = { name: 'this is diff', childrens: getArrayOfDiff(valueOfFirstFile, valueOfSecondFile) };
   return treeOfDiff;
 };
 
-const getStatusOfKey = (key) => {
-  const status = [];
-  if (key.startsWith('|added|')) status.push('added');
-  if (key.startsWith('|removed|')) status.push('removed');
-  if (key.startsWith('|updated/removed|')) status.push('updated/removed');
-  if (key.startsWith('|updated/added|')) status.push('updated/added');
-  if (status.length === 0) return 'unchanged';
-  return status[0];
-};
+const getStatusOfNode = (node) => node.status;
 
-const getDiffAsObj = (tree) => {
-  const obj = _.cloneDeep(tree);
-  return obj;
-};
+const getKeyOfNode = (node) => node.key;
 
+const getChildrensOfNodeAsArray = (node) => node.childrens;
+
+const getChildOfChildrens = (array, statusOfUpdatedKey = null) => {
+  let value = 'uncorrectStatus';
+  //console.log(array[0]);
+  if (statusOfUpdatedKey === null) [value] = array;
+  if (statusOfUpdatedKey === 'added') [, value] = array;
+  if (statusOfUpdatedKey === 'removed') [value] = array;
+  return value;
+};
+/*
 const getArrayOfKeysStatusesValuesOfFirstLevelNesting = (diffAsObj) => {
   const result = Object.entries(diffAsObj).map(([key, value]) => {
     const status = getStatusOfKey(key);
@@ -62,20 +68,22 @@ const getArrayOfKeysStatusesValuesOfFirstLevelNesting = (diffAsObj) => {
   return result;
 };
 
-const getArrayOfObjWithPathStatusValueOfAllChangedKeys = (meaning, status = 'unchanged', path = '') => {
-  if (status === 'added' || status === 'removed') return { path, status, meaning };
-  if (status === 'updated/added' || status === 'updated/removed') return { path, status, meaning };
-  const arrayWithPathsStatusesValues = getArrayOfKeysStatusesValuesOfFirstLevelNesting(meaning)
-    .flatMap(([key, status2, value]) => {
-      const newPath = `${path}${key}.`;
+const getArrayOfObjWithPathStatusValueOfAllChangedKeys =
+ (node, status = 'unchanged', path = '') => {
+  if (status === 'added' || status === 'removed')
+  return { path, status, getChildOfChildrens(node) };
+  if (status === 'updated') return { path, status, meaning };
+  const arrayWithPathsStatusesValues = getChildrensOfNodeAsArrayOfobj(node)
+    .flatMap(({ name, status, childrens }) => {
+      const newPath = `${path}${name}.`;
       if (!_.isObject(value) && status2 === 'unchanged') return [];
       return getArrayOfObjWithPathStatusValueOfAllChangedKeys(value, status2, newPath);
     });
   return arrayWithPathsStatusesValues;
 };
-
+*/
 export {
-  getStatusOfKey, diff,
-  getArrayOfKeysStatusesValuesOfFirstLevelNesting,
-  getDiffAsObj, getArrayOfObjWithPathStatusValueOfAllChangedKeys,
+  diff, getStatusOfNode,
+  getKeyOfNode, getChildrensOfNodeAsArray,
+  getChildOfChildrens,
 };
