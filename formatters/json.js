5053517dada10ed1098432baef574b50033fae61
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-09 18:32:27 +0200

    edit clone diff

--- formatters/json.js
+++ formatters/json.js
@@ -1,13 +1,4 @@
 import _ from 'lodash';
-import { getChildrensOfNodeAsArray } from '../src/diff.js';
 
-const json = (diffAsTree) => {
-  const childrensOfdiff = getChildrensOfNodeAsArray(diffAsTree);
-  const result = childrensOfdiff.map((item) => _.cloneDeep(item));
-
-  return {
-    name: 'this is diff',
-    childrens: result,
-  };
-};
+const json = (diffAsTree) => _.cloneDeep(diffAsTree);
 export default json;
