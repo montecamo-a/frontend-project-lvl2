bc190c0ce74510b327c441e441c463f4a5feb997
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-09 18:16:59 +0200

    added condition when childrens.length === 0

--- formatters/plain.js
+++ formatters/plain.js
@@ -10,36 +10,41 @@ const plain = (diffAsTree) => {
   const valueFormatter = (value) => {
     if (_.isString(value)) return `'${value}'`;
     if (_.isObject(value)) return '[complex value]';
-    return value;
+    return `${value}`;
   };
 
   const arrayFormatter = (array, paths = '') => {
     const child = getFirstChildOfChildrens(array);
     if (!_.isObject(child)) return [];
 
-    const arrayOfStrings = array
-      .flatMap(({ name, status, childrens }) => {
-        const newPath = paths.length === 0 ? name : `${paths}.${name}`;
-        switch (status) {
-          case 'updated':
-            return `Property '${newPath}' was updated. From`
+    const arrayOfStrings = array.flatMap(({ name, status, childrens }) => {
+      const newPath = paths.length === 0 ? name : `${paths}.${name}`;
+      switch (status) {
+        case 'updated':
+          return `Property '${newPath}' was updated. From`
                   + ` ${valueFormatter(getRemovedChildOfUpdatedNode(childrens))} to`
                   + ` ${valueFormatter(getAddedChildOfUpdatedNode(childrens))}\n`;
 
-          case 'added':
-            return `Property '${newPath}' was added with value: ${valueFormatter(getFirstChildOfChildrens(childrens))}\n`;
+        case 'added':
+          return `Property '${newPath}' was added with value: ${valueFormatter(getFirstChildOfChildrens(childrens))}\n`;
 
-          case 'removed':
-            return `Property '${newPath}' was removed\n`;
+        case 'removed':
+          return `Property '${newPath}' was removed\n`;
 
-          default:
-            return arrayFormatter(childrens, newPath);
-        }
-      });
+        default:
+          return arrayFormatter(childrens, newPath);
+      }
+    });
 
     return arrayOfStrings;
   };
-  return arrayFormatter(getChildrensOfNodeAsArray(diffAsTree)).join('');
+  const childrens = getChildrensOfNodeAsArray(diffAsTree);
+  switch (childrens.length) {
+    case 0:
+      return '';
+    default:
+      return arrayFormatter(getChildrensOfNodeAsArray(diffAsTree)).join('');
+  }
 };
 
 export default plain;
