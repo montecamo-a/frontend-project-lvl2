d1252568e122ea61e16d5fbf360b5b7299e49f14
Bulat Masalimov <bmasalimov5@gmail.com>
2022-02-09 18:54:00 +0200

    rm mutating

--- src/diff.js
+++ src/diff.js
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
 import _ from 'lodash';
 
 const diff = (valueOfFirstFile, valueOfSecondFile) => {
@@ -13,30 +15,26 @@ const diff = (valueOfFirstFile, valueOfSecondFile) => {
 
     const keysOfNodeOfFirstFile = Object.keys(nodeOfFirstFile);
     const keysOfNodeOfSecondFile = Object.keys(nodeOfSecondFile);
-    const allKeys = _.uniq([...keysOfNodeOfFirstFile, ...keysOfNodeOfSecondFile]).sort();
+    const allKeys = _.uniq([...keysOfNodeOfFirstFile, ...keysOfNodeOfSecondFile]).slice().sort();
 
-    const partOfTree = allKeys.reduce((acc, key) => {
+    const partOfTree = allKeys.map((key) => {
       if (_.has(nodeOfFirstFile, key) && _.has(nodeOfSecondFile, key)) {
         if (_.isObjectLike(nodeOfFirstFile[key]) && _.isObjectLike(nodeOfSecondFile[key])) {
-          acc.push({ name: `${key}`, status: 'unchanged', childrens: getArrayOfDiff(nodeOfFirstFile[key], nodeOfSecondFile[key]) });
-          return acc;
+          return { name: `${key}`, status: 'unchanged', childrens: getArrayOfDiff(nodeOfFirstFile[key], nodeOfSecondFile[key]) };
         }
 
         const isUnchanged = checkIsUnchanged(nodeOfFirstFile[key], nodeOfSecondFile[key]);
         if (!isUnchanged) {
-          acc.push({ name: `${key}`, status: 'unchanged', childrens: getArrayOfDiff(nodeOfFirstFile[key], nodeOfFirstFile[key]) });
-          return acc;
+          return { name: `${key}`, status: 'unchanged', childrens: getArrayOfDiff(nodeOfFirstFile[key], nodeOfFirstFile[key]) };
         }
         if (isUnchanged) {
-          acc.push({ name: `${key}`, status: 'updated', childrens: [getArrayOfDiff(nodeOfFirstFile[key], nodeOfFirstFile[key]), getArrayOfDiff(nodeOfSecondFile[key], nodeOfSecondFile[key])].flat() });
-          return acc;
+          return { name: `${key}`, status: 'updated', childrens: [getArrayOfDiff(nodeOfFirstFile[key], nodeOfFirstFile[key]), getArrayOfDiff(nodeOfSecondFile[key], nodeOfSecondFile[key])].flat() };
         }
       }
 
-      if (_.has(nodeOfFirstFile, key)) acc.push({ name: `${key}`, status: 'removed', childrens: getArrayOfDiff(nodeOfFirstFile[key], nodeOfFirstFile[key]) });
-      if (_.has(nodeOfSecondFile, key)) acc.push({ name: `${key}`, status: 'added', childrens: getArrayOfDiff(nodeOfSecondFile[key], nodeOfSecondFile[key]) });
-      return acc;
-    }, []);
+      if (_.has(nodeOfFirstFile, key)) return { name: `${key}`, status: 'removed', childrens: getArrayOfDiff(nodeOfFirstFile[key], nodeOfFirstFile[key]) };
+      if (_.has(nodeOfSecondFile, key)) return { name: `${key}`, status: 'added', childrens: getArrayOfDiff(nodeOfSecondFile[key], nodeOfSecondFile[key]) };
+    });
     return partOfTree;
   };
   const treeOfDiff = { name: 'this is diff', childrens: getArrayOfDiff(valueOfFirstFile, valueOfSecondFile) };
